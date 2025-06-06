e2b087ff3a53a5f31ece24973a34007382a25a7d
Samarth <swaghela@myseneca.ca>
2025-06-06 16:15:32 -0400

    Individual message or note.

---
+++ lab4d.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Strings 1
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(input_str):
+    # Returns the first five characters of the string
+    return input_str[:5]
+
+def last_seven(input_str):
+    # Returns the last seven characters of the string
+    return input_str[-7:]
+
+def middle_number(num):
+    # Converts number to string and returns 2nd and 3rd characters
+    return str(num)[1:3]
+
+def first_three_last_three(str_a, str_b):
+    # Combines first 3 chars of str_a and last 3 chars of str_b
+    return str_a[:3] + str_b[-3:]
+
+if __name__ == '__main__':
+    print(first_five(str1))
+    print(first_five(str2))
+    print(last_seven(str1))
+    print(last_seven(str2))
+    print(middle_number(num1))
+    print(middle_number(num2))
+    print(first_three_last_three(str1, str2))
+    print(first_three_last_three(str2, str1))
+
